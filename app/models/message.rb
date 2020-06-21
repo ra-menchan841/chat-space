43c58a1335f86205ad0df4c9cc0104e65e7e657a
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-06-21 23:20:52 +0900

    edit form &link in main.html.haml

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ApplicationRecord
-  belobgs_to :group
+  belongs_to :group
   belongs_to :user
 
   validates :content, presence: true, unless: :image?
 
-  mount_uploader :image, ImageUploader
+  
 end
