b7c1a4318dd47bdbba17633e1a14a635d635850c
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-07-04 17:10:46 +0900

    create jbuilder file

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,13 @@
 $(function(){
+  function buildHTML(message){
+    // 「もしメッセージに画像が含まれていたら」という条件式
+    if (message.image) {
+      var html = //メッセージに画像が含まれる場合のHTMLを作る
+    } else {
+      var html = //メッセージに画像が含まれない場合のHTMLを作る
+    }
+    return html
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
