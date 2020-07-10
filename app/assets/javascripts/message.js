87195807591f38d07e2c9025560ab6eb2265a260
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-07-10 17:14:34 +0900

    add details of function for ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,9 @@
 $(function(){
+
   function buildHTML(message){
     if (message.image) {
       var html =
-        `<div class="main-message-list__message">
+        `<div class="main-message-list__message" data-message-id=${message.id}>
           <ul class="main-message-list__message__user-name">
             <li class="user-name">
             ${message.user_name}
@@ -23,7 +24,7 @@ $(function(){
       return html;
     } else {
       var html =
-        `<div class="main-message-list__message">
+        `<div class="main-message-list__message" data-message-id=${message.id}>
           <ul class="main-message-list__message__user-name">
             <li class="user-name">
             ${message.user_name}
@@ -66,4 +67,34 @@ $(function(){
         $('.main-form__box__send').prop("disabled", false);
       });
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
+
